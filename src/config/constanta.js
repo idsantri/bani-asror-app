ed158dc35c54fa70134bca650808772f63ec9e66
Usman <oets.man@gmail.com>
2023-03-22 22:01:08 +0700

    .

--- src/config/constanta.js
+++ src/config/constanta.js
@@ -2,7 +2,6 @@ const constanta = {
   ANCESTOR: "Kiai Asror",
   ANCESTOR_ALIAS: "Bujuk Langgundih",
   APP_NAME_1: "Silsilah",
-  APP_NAME_2: "Bani Asror",
-  APP_NAME_FULL: "Silsilah Bani Asror"
+  APP_NAME_2: "Bani Asror"
 };
 export default constanta;
