058557fee69a10a341157a5ffb8ec5254d463063
Usman <oets.man@gmail.com>
2023-03-24 03:21:07 +0700

    pwa success

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -26,6 +26,14 @@
         <q-btn-dropdown flat round dense dropdown-icon="more_vert" class="q-pl-md" color="green-1">
 
           <q-list>
+
+            <q-item clickable v-close-popup name="install" @click="installApp" v-if="!pwaIsInstalled">
+              <q-item-section>Install</q-item-section>
+              <q-item-section avatar>
+                <q-icon color="green" name="install_mobile" />
+              </q-item-section>
+            </q-item>
+
             <q-item clickable v-close-popup to="/profile">
               <q-item-section>Profil</q-item-section>
               <q-item-section avatar>
@@ -119,7 +127,7 @@
 </template>
 
 <script setup>
-import { ref, watchEffect } from "vue";
+import { ref, watchEffect, computed, onMounted } from "vue";
 import AsideContent from "src/components/AsideContent.vue";
 import MemberDataTable from '../components/MemberDataTables.vue'
 import { showModalSearch, showModalCrud } from 'src/utils/buttons-click';
@@ -162,6 +170,34 @@ watchEffect(() => {
   }
 })
 
+/**
+ * ----------------------------------
+ * PWA
+ * ----------------------------------
+ */
+let deferredPrompt = ref(null)
+onMounted(async () => {
+  window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault()
+    deferredPrompt.value = e
+  })
+})
+const pwaIsInstalled = computed(() => {
+  if (window.matchMedia('(display-mode:standalone)').matches) {
+    console.log('this is standalone');
+    return true
+  }
+  console.log('this is not standalone');
+  return false
+})
+const installApp = async () => {
+  deferredPrompt.value.prompt()
+  const { outcome } = await deferredPrompt.value.userChoice
+  if (outcome === 'dismissed') {
+    console.log('this is standalone');
+    return true
+  }
+}
 </script>
 <style scoped>
 .spinner {
