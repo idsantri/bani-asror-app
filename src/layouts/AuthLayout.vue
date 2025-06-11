fdfcc4008a2a4a2cb7f4977db9c605758887eb49
oets <oets.man@gmail.com>
2025-06-11 21:11:18 +0700

    pwa on login + screen shot

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -2,9 +2,37 @@
 	<div class="absolute-center full-width q-px-md">
 		<div style="max-width: 400px; margin: auto">
 			<!-- PWA -->
-			<!-- <pwa-card /> -->
-
-			<!-- MAIN CARD -->
+			<q-card
+				v-if="!isPwaInstalled && isMobile()"
+				class="row q-mb-md bg-green-11 text-green-10"
+			>
+				<q-item class="">
+					<q-item-section>
+						<q-item-label>
+							Install aplikasi ini ke perangkat Anda untuk
+							pengalaman yang lebih baik.
+						</q-item-label>
+					</q-item-section>
+					<q-item-section avatar="">
+						<q-btn
+							v-if="isAndroid()"
+							icon="install_mobile"
+							round
+							class="text-green-10 bg-green-13"
+							glossy
+							@click="installPwa"
+						/>
+						<q-btn
+							v-if="isIos()"
+							icon="install_mobile"
+							round
+							class="text-green-10 bg-green-13"
+							glossy
+							@click="clickIos"
+						/>
+					</q-item-section>
+				</q-item>
+			</q-card>
 			<q-card
 				class="bg-secondary text-green-1 q-pa-lg bg-green-7 text-green-1"
 			>
@@ -44,6 +72,10 @@
 					<router-view @title="handleTitle" @errors="handleErrors" />
 				</q-card-section>
 			</q-card>
+			<!-- INSTALL PWA -->
+			<q-dialog v-model="modalIos">
+				<InfoIos />
+			</q-dialog>
 		</div>
 	</div>
 </template>
@@ -51,12 +83,68 @@
 <script setup>
 import config from 'src/config';
 import { ref } from 'vue';
-// import PwaCard from 'src/components/PwaCard.vue';
+import InfoIos from './comp/InfoIos.vue';
+import { QCard } from 'quasar';
+
 const title = ref('Autentikasi');
 const handleTitle = (value) => (title.value = value);
+const modalIos = ref(false);
 
 const errors = ref([]);
 const handleErrors = (value) => (errors.value = value);
+
+/**
+ * -----------------------------------------
+ * PWA
+ * -----------------------------------------
+ */
+
+const getOS = () => {
+	const userAgent = window.navigator.userAgent;
+	const osList = [
+		{ regex: /windows/i, name: 'Windows' },
+		{ regex: /android/i, name: 'Android' },
+		{ regex: /(iphone|ipad|ipod)/i, name: 'iOS' },
+		{ regex: /linux/i, name: 'Linux' },
+		{ regex: /mac/i, name: 'Mac/iOS' },
+	];
+
+	return (
+		osList.find(({ regex }) => regex.test(userAgent))?.name || 'Unknown OS'
+	);
+};
+const isIos = () => getOS() === 'iOS';
+const isAndroid = () => getOS() === 'Android';
+const isMobile = () => isIos() || isAndroid();
+
+const deferredPrompt = ref(null);
+window.addEventListener('beforeinstallprompt', (e) => {
+	e.preventDefault();
+	deferredPrompt.value = e;
+});
+
+const isPwaInstalled = ref(
+	window.matchMedia('(display-mode: standalone)').matches,
+);
+
+const installPwa = () => {
+	if (deferredPrompt.value) {
+		deferredPrompt.value.prompt();
+		deferredPrompt.value.userChoice.then((choiceResult) => {
+			if (choiceResult.outcome === 'accepted') {
+				console.log('User accepted the A2HS prompt');
+			} else {
+				console.log('User dismissed the A2HS prompt');
+			}
+		});
+		// reset
+		deferredPrompt.value = null;
+	}
+};
+
+const clickIos = () => {
+	modalIos.value = true;
+};
 </script>
 
 <style scoped>
---
+++ src/layouts/comp/InfoIos.vue
@@ -0,0 +1,53 @@
+<template lang="">
+	<q-card style="min-width: 300px">
+		<q-card-section
+			class="q-py-sm q-px-md bg-green-6 text-green-11 text-subtitle1"
+		>
+			Instal Aplikasi
+		</q-card-section>
+		<q-card-section class="q-py-sm q-px-md bg-green-1 text-green-10">
+			<p class="no-margin">
+				Instal aplikasi untuk mendapatkan pengalaman lebih baik!
+			</p>
+			<q-separator class="q-my-sm" />
+			<p class="no-margin">Bagi Pengguna iOS!</p>
+			<ol class="no-margin q-pl-md">
+				<li>
+					Pastikan Anda menggunakan browser
+					<span class="text-weight-bold">Safari</span>;
+				</li>
+				<li>
+					Ketuk ikon &ldquo;bagikan&rdquo; (sebuah kotak dengan panah
+					ke atas di bagian bawah layar);
+				</li>
+				<li>
+					Gulir ke bawah dalam menu bagikan dan pilih opsi
+					&ldquo;Tambahkan ke Layar Utama&rdquo;;
+				</li>
+				<li>
+					Anda mungkin akan diminta untuk memberikan nama dan
+					mengonfigurasi ikon;
+				</li>
+				<li>
+					Ketuk &ldquo;Tambahkan&rdquo; di pojok kanan atas layar
+					untuk menyelesaikan proses;
+				</li>
+			</ol>
+		</q-card-section>
+		<q-card-actions align="right" class="bg-green-5 q-py-sm q-px-md">
+			<q-btn
+				outline
+				no-caps
+				label="Tutup"
+				color="green-11"
+				class="bg-green-6"
+				glossy
+				v-close-popup
+			/>
+		</q-card-actions>
+	</q-card>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
