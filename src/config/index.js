bd98246a8b71b163e1c7b6055ce34c851507dcb3
idsantri <idsantri.mail@gmail.com>
2026-08-20 16:51:07 +0700

    fix: update BASE_API URL in config to correct endpoint

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,6 @@ const config = {
 	ANCESTOR_ALIAS: 'Bujuk Langgundih',
 	APP_NAME_1: 'Silsilah',
 	APP_NAME_2: 'Bani Asror',
-	BASE_API: 'https://api-ba.baniasror.net',
+	BASE_API: 'https://ba.idsantri.my.id',
 };
 export default config;
